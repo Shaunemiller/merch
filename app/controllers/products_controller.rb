3491232b137bdb8519fd624efb87cc076165172c
shaun <shaun>
2015-01-21 16:17:36 +0000

    define routes; create template; define actions

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,32 @@
+class ProductsController < ApplicationController
+	def index
+	end
+
+	def show
+	end
+
+	def new
+	end
+
+	def create
+	end
+
+	def edit
+	end
+
+	def update
+	end
+
+	def destroy
+	end
+
+	private
+	def product_params
+		#whitelisted form data
+	end
+
+	def find_product
+		#query to read a record from the DB
+	end
+	
+end
