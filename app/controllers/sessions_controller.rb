227343d97a350a168ca3066a05c4695fc8843c29
shaun <shaun>
2015-01-22 17:12:38 +0000

    password; login

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,45 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+
+  def create
+  # get email from params
+  email = params[:session][:email]
+  # get password from params 
+  password = params[:session][:password]
+  # Get User from DB using email from params
+  @user = User.find_by(email: email)
+  # compare pw from params with pw from db
+  if @user.present? and @user.authenticate(password)
+    # login
+    # unset any session data (in case of session hijacking)
+    reset_session
+
+    #log the user in 
+    session[:user_id] = @user.id
+    
+    #tell them it worked
+    flash[:success] = "Welcome back #{@user.email}."
+ 	 
+    #send them home
+ 	redirect_to root_path
+  else
+  	#tell them it didn't work
+  	flash[:error] = "Sorry it didn't work!"
+ 	
+ 	 render :new
+  end
+
+ end
+
+
+
+
+
+
+
+
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  
-  root "products#index"
+   root "products#index"
   resources :products
   resources :users
+  # resource and session are singular because you only have one session at a time 
+  resource :session, only: [:new, :create, :destroy]
 end
