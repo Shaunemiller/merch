1c0759cc0ab1cad266cbc9d2e7db1180af9c8f0b
shaun <shaun>
2015-01-22 16:30:08 +0000

    updates

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,19 +14,46 @@ before_action :find_user, {only:
   end
 
   def create
+  		@user = User.new(user_params)
+  		if @user.save
+  		flash[:success] = "'#{@user.email}' profile was created."
+		redirect_to user_path(@user)
+
+		else
+			flash[:error] = "Oops, something went wrong, check the form and try again."
+			render :new
+		end
   end
 
   def edit
   end
 
   def update
+  	if @user.update(user_params
+			)
+			flash[:success] = "'#{@user.email}' was updated successfully."
+			redirect_to user_path(@user)
+		else
+			flash[:error] = "Oops, something went wrong, check the form and try again."
+			render :edit
+		end
+
   end
 
   def destroy
+  	@user.destroy
+		flash[:success] = "'#{@user.email}' was deleted."
+		@user = nil
+		redirect_to root_path
   end
 
 
   private
+  def user_params
+
+  	params.require(:user).permit(:email, :password, :password_confirmation)
+  end
+
   def find_user
   		@user = User.find(params["id"])
   	end
